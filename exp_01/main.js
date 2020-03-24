6aed57788307b7efe9e8965ff89e2fac0d2990ea
kakapiya <3076619842@qq.com>
2020-03-24 12:19:43 +0800

    npm ERR! missing script: start

---
+++ exp_01/main.js
@@ -0,0 +1,51 @@
+const electron = require('electron')
+
+//1.将在应用程序准备就绪后打开一个窗并且打开开发者工具
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {   
+  // 创建浏览器窗口
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // 加载index.html文件
+  win.loadFile('index.html')
+
+  
+ // 打开开发者工具
+ win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+
+//2.处理窗口关闭事件、在macOS用户点击dock上图标时重建窗口
+
+
+// 部分 API 在 ready 事件触发后才能使用。
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+  // 否则绝大部分应用及其菜单栏会保持激活。
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+  // 通常在应用程序中重新创建一个窗口。
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. 也可以拆分成几个文件，然后用 require 导入。
